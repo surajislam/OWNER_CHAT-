3b6711d95ddf240aac574579eb74f8c40a7fd126
surajislam <154444093+surajislam@users.noreply.github.com>
2024-06-13 00:25:25 +0530

    Update inline.py

--- ROYEDITX/modules/helpers/inline.py
+++ ROYEDITX/modules/helpers/inline.py
@@ -11,7 +11,7 @@ DEV_OP = [
         ),
     ],
     [
-        InlineKeyboardButton(text="ʀᴇᴘᴏ", callback_data="SOURCE"),
+        InlineKeyboardButton(text="Nᴘᴏ", callback_data="SOURCE"),
         InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{SUPPORT_GRP}"),
     ],
     [
